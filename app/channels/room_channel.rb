c70eedc712545b55dbf03adb72b92cf1dd78a047
TakaiSaisei <takaisaisei@gmail.com>
2021-01-13 22:39:39 +0300

    Don't try to subscribe if there is no opened room

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -2,10 +2,16 @@
 
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "room_channel_#{params[:room_id]}"
+    channel = Room.find_by(params[:room_id])
+
+    if channel
+      stream_from "room_channel_#{channel}"
+    else
+      reject
+    end
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
   end
 end
